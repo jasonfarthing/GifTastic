cfb47c990c90cb16154aeba5df15abb89b88f097
jasonfarthing <jasonfarthingisnow@gmail.com>
2017-11-06 15:32:23 -0800

    render buttons

--- gif.js
+++ gif.js
@@ -4,26 +4,42 @@
 
 var instrumentData = ["Guitar", "Banjo", "Tuba", "Piano"];
 
-$("button").click(function(){
+$("button").click(function () {
     var instInput = $(this).data("instrument");
     console.log(instInput);
-    
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + instInput + "&api_key=uTMqATcVmg0xGsWfo44I5hstyO0P39Gv&rating=G&limit=10";
 
-        $.ajax({
-            url: queryURL,
-            method: "GET",
-        }).done(function(response){
-            var instrumentData = response.data;
-            for (i = 0; i < instrumentData.length; i++);
-            console.log(response);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + instInput + "&api_key=uTMqATcVmg0xGsWfo44I5hstyO0P39Gv&rating=G&limit=10";
 
-            $()
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).done(function (response) {
+        var instrumentData = response.data;
+        for (i = 0; i < instrumentData.length; i++);
+        console.log(response);
 
-            
-        })
+       
+
+
+    })
 })
 
+
+
+function renderButtons() {
+
+    $("#instrButton").empty();
+
+    for (var i = 0; i < instrumentData.length; i++) {
+
+        var a = $("<button>");
+        a.addClass("instrument");
+        a.attr("data-name", instrumentData[i]);
+        a.text(instrumentData[i]);
+        $("#instrButton").append(a);
+    }
+}
+renderButtons();
 
 
 
